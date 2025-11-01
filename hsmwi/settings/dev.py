2ef5a71cee986a4cfc30b95a3b5c59a2521eac3a
jobet1995 <jobetcasquejo221@gmail.com>
2025-11-01 19:17:44 +0800

    initialize

---
+++ hsmwi/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-=y&+x6o^ox*7ms64=8r+91+4dte*4ku%h^uol0x60sz6@9xmrr"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
